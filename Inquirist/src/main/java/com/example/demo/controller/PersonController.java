9f2d27ccd9a5c93cbf85631322553b016d1fa150
Léon Muller <leonmuller@hotmail.fr>
2022-03-23 11:04:30 +0100

    Refactored some code

--- Inquirist/src/main/java/com/example/demo/controller/PersonController.java
+++ Inquirist/src/main/java/com/example/demo/controller/PersonController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -15,12 +17,13 @@ import com.example.demo.model.Person;
 import com.example.demo.repository.PersonsRepository;
 
 @Controller
+@RequestMapping("/persons")
 public class PersonController
 {	
 	@Autowired
 	PersonsRepository personsRepo;
 	
-	@GetMapping("/persons")
+	@GetMapping
 	public ModelAndView persons()
 	{
 		ModelAndView mav = new ModelAndView();
@@ -28,28 +31,28 @@ public class PersonController
 		return mav;
 	}
 	
-	@GetMapping("persons/{id}")
+	@GetMapping("/{id}")
 	public String detail(@PathVariable("id") long id, Map<String, Object> model)
 	{
 		model.put("person", personsRepo.findById(id).get());
 		return "person_detail";
 	}
 
-	@RequestMapping("persons/create")
+	@GetMapping("/create")
 	public String create(@ModelAttribute(value="person") Person person, Map<String, Object> model)
 	{
 		model.put("person", new Person());
 		return "person_create";
 	}
 	
-	@RequestMapping("persons/insert")
+	@PostMapping("/insert")
 	public RedirectView insert(@ModelAttribute(value="person") Person person, Map<String, Object> model)
 	{
 		personsRepo.save(person);
 		return new RedirectView("/persons");
 	}
 	
-	@GetMapping("persons/{id}/remove")
+	@GetMapping("/{id}/remove")
 	public RedirectView empDelete2(@PathVariable("id") long id, Map<String, Object> model)
 	{
 		personsRepo.deleteById(id);
