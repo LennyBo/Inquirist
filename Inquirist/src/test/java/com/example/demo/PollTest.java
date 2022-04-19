668ee135e9a7d568156c94a51933165d6ddb9510
Reeezer <leonmuller@hotmail.fr>
2022-04-19 18:12:02 +0200

    Added forgotten tests conflicts

--- Inquirist/src/test/java/com/example/demo/PollTest.java
+++ Inquirist/src/test/java/com/example/demo/PollTest.java
@@ -8,54 +8,51 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.demo.model.Poll;
-import com.example.demo.model.User;
 import com.example.demo.repository.PollsRepository;
 import com.example.demo.repository.UsersRepository;
 
-
 @SpringBootTest
-public class PollTest {
+public class PollTest
+{
 	@Autowired
 	UsersRepository usersRepo;
-	
+
 	@Autowired
 	PollsRepository pollsRepo;
-	
-	  @Test
-	  public void testParseAnswersStringList() 
-	  {
-		  Poll testPoll = new Poll();
-		  
-		  testPoll.setAnswersString("oui;non;");
-		  String answers[] = {"oui", "non"};
-		  String testAnswers[] = testPoll.getAnswersStringList();
-		  Assertions.assertArrayEquals(answers, testAnswers);
-		  
-		  
-		  testPoll.setAnswersString("oui;non;peu etre;");
-		  String answers2[] = {"oui", "non", "peu etre"};
-		  String testAnswers2[] = testPoll.getAnswersStringList();
-		  Assertions.assertArrayEquals(answers2, testAnswers2);
-	  }
 
-	  @Test
-	  public void testInsertIntoDb()
-	  {
-		  Poll testPoll = new Poll("test poll ?", "my Test Poll", null);
-		  Poll savedPoll = pollsRepo.save(testPoll);
-	  }
-	  
-	  @Test
-	  public void testFindPollById()
-	  {
-		  Poll testPoll = new Poll("test poll ?", "my Test Poll", null);
-		  Poll savedPoll = pollsRepo.save(testPoll);
-		  
-		  Optional<Poll> dbPoll = pollsRepo.findById(savedPoll.getId());
-		  Assertions.assertEquals(savedPoll.getDescription(), dbPoll.get().getDescription());
-		  Assertions.assertEquals(savedPoll.getTitle(), dbPoll.get().getTitle());
+	@Test
+	public void testParseAnswersStringList()
+	{
+		Poll testPoll = new Poll();
 
-	  }
-	  
-	  
+		testPoll.setAnswersString("oui;non;");
+		String answers[] = { "oui", "non" };
+		String testAnswers[] = testPoll.getAnswersStringList();
+		Assertions.assertArrayEquals(answers, testAnswers);
+
+		testPoll.setAnswersString("oui;non;peu etre;");
+		String answers2[] = { "oui", "non", "peu etre" };
+		String testAnswers2[] = testPoll.getAnswersStringList();
+		Assertions.assertArrayEquals(answers2, testAnswers2);
+	}
+
+	@Test
+	public void testInsertIntoDb()
+	{
+		Poll testPoll = new Poll("test poll ?", "my Test Poll", null);
+		Poll savedPoll = pollsRepo.save(testPoll);
+	}
+
+	@Test
+	public void testFindPollById()
+	{
+		Poll testPoll = new Poll("test poll ?", "my Test Poll", null);
+		Poll savedPoll = pollsRepo.save(testPoll);
+
+		Optional<Poll> dbPoll = pollsRepo.findById(savedPoll.getId());
+		Assertions.assertEquals(savedPoll.getDescription(), dbPoll.get().getDescription());
+		Assertions.assertEquals(savedPoll.getTitle(), dbPoll.get().getTitle());
+
+	}
+
 }
--- Inquirist/src/test/java/com/example/demo/UserTest.java
+++ Inquirist/src/test/java/com/example/demo/UserTest.java
@@ -7,46 +7,43 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.example.demo.model.Poll;
+import com.example.demo.model.Role;
 import com.example.demo.model.User;
-import com.example.demo.repository.PollsRepository;
 import com.example.demo.repository.UsersRepository;
 
 @SpringBootTest
-public class UserTest {
+public class UserTest
+{
 	@Autowired
 	UsersRepository usersRepo;
-	
+
 	@Test
 	public void testInsertIntoDb()
 	{
-		User testUser = new User("user11", "mat33", "mdp", true);
-		usersRepo.save(testUser);	
+		User testUser = new User("user11", "mat33", "mdp", "mdp", Role.READER);
+		usersRepo.save(testUser);
 	}
-	
+
 	@Test
 	public void testFindById()
 	{
-		User testUser = new User("user111", "mat32", "mdp", true);
-		long id = usersRepo.save(testUser).getId();		
-		
-		  Optional<User> dbUser = usersRepo.findById(id);
-		  Assertions.assertEquals(testUser.getName(), dbUser.get().getName());
-		  Assertions.assertEquals(testUser.getUsername(), dbUser.get().getUsername());
+		User testUser = new User("user111", "mat32", "mdp", "mdp", Role.READER);
+		long id = usersRepo.save(testUser).getId();
 
+		Optional<User> dbUser = usersRepo.findById(id);
+		Assertions.assertEquals(testUser.getName(), dbUser.get().getName());
+		Assertions.assertEquals(testUser.getUsername(), dbUser.get().getUsername());
 	}
-	
+
 	@Test
 	public void testFindUserByUserame()
 	{
-		User testUser = new User("user112", "mat133", "mdp", true);
-		User dbUser  = usersRepo.save(testUser);		
+		User testUser = new User("user112", "mat133", "mdp", "mdp", Role.READER);
+		User dbUser = usersRepo.save(testUser);
 		User findUser = usersRepo.findByUsername("user112");
-		
+
 		Assertions.assertEquals(findUser.getId(), dbUser.getId());
 		Assertions.assertEquals(findUser.getUsername(), dbUser.getUsername());
 		Assertions.assertEquals(findUser.getName(), dbUser.getName());
-
 	}
-	
 }
